3c81faa7d0460f3c1cfaf01e90831e1d7a5f91dd
prasanna <uiintern.infoxit@gmail.com>
2021-12-25 13:39:46 +0545

    test2

--- app.py
+++ app.py
@@ -10,8 +10,6 @@ import streamlit as st
 start= '2010-01-01'
 end='2019-12-31'
 
-
-
 st.title('Stock Market Prediction Using Machine Learning')
 
 user_input=st.text_input('Enter Stock Ticker','AAPL')
