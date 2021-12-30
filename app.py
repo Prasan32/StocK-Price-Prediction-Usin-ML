091c61f7ecf8e487eebfcbffe47f67b856ab36a8
prasanna <uiintern.infoxit@gmail.com>
2021-12-30 14:48:13 +0545

    newui

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ st.markdown(f""" <style>
     .reportview-container .main .block-container{{
         padding-top: {padding}rem;
         padding-right: {padding}rem;
-        padding-left: {padding}rem;
+        padding-left: 1 rem;
         padding-bottom: {padding}rem;
     }} </style> """, unsafe_allow_html=True)
 
